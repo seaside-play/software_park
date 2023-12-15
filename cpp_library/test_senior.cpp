def19eef61df613124a31c4c00c6d99b503c9688
Chris <namtaerg@gmail.com>
2023-12-15 10:09:20 +0800

    test senior

---
+++ cpp_library/test_senior.cpp
@@ -0,0 +1,34 @@
+#include "test_senior.h"
+
+#include <iostream>
+#include <memory>
+#include <type_traits>
+
+namespace test {
+
+struct Net {
+
+};
+
+using SpNet = std::shared_ptr<Net>;
+
+template <typename T, std::enable_if<std::is_same<T, int>::value>>
+void FillNet(T i) {
+  std::cout << "int parameter\n" ;
+}
+
+template <typename T, std::enable_if<std::is_same<T, int>::value>>
+void FillNet(double d) {
+  std::cout << "double parameter\n" ;
+}
+
+void TestSenior::Test() {
+  TestEnableIf();
+}
+
+
+void TestSenior::TestEnableIf() {
+
+}
+
+}  // namespace test
---
+++ cpp_library/test_senior.h
@@ -0,0 +1,16 @@
+#ifndef TRAIN_TEST_SENIOR_H_
+#define TRAIN_TEST_SENIOR_H_
+
+namespace test {
+
+class TestSenior {
+ public:
+  void Test();
+
+ private:
+  void TestEnableIf();
+};
+
+}  // namespace test
+
+#endif  // TRAIN_TEST_SENIOR_H_
